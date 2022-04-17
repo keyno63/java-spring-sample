40990185767ff8744018a6a95c81fdc7020d9259
Keyno <kofujiw@gmail.com>
2022-04-17 23:59:04 +0900

    Initial commit

---
+++ src/main/java/com/example/javaspringsample/JavaSpringSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaspringsample/JavaSpringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
